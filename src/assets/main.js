9e4257dab9c90cdc0df1e5c33c48504eb3aec59a
yyan-prtech <yyan@prtech.com>
2017-10-15 20:35:52 -1000

    Check Codes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+      url: 'https://www.codeschool.com/users/yyan_prtech.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        // handle response
+        result = response.courses.completed;
+        $.each( result, function( i, val ) {
+          $( "#badges").append( "<div class=\"course\"><h3>" + val.title + "</h3><img src=\"" + val.badge + "\"><a class=\"btn btn-primary\" href=\"" + val.url + "\" target=\"_blank\">See Course</a></div>" );
+        });
+      }
+    });
 });
